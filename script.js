a298041f69e54585a131f103b936d6ddbca67e1f
parwer <55444278+parwer@users.noreply.github.com>
2022-10-18 19:20:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function show_profile() {
+    document.getElementById("show profile").style.zIndex = "2";
+    document.getElementById("show experiences").style.zIndex = "-1";
+    document.getElementById("show abilities").style.zIndex = "-1";
+    document.getElementById("show project").style.zIndex = "-1";
+    document.getElementById("show contact").style.zIndex = "-1";
+  }
+function show_experiences() {
+    document.getElementById("show profile").style.zIndex = "-1";
+    document.getElementById("show experiences").style.zIndex = "2";
+    document.getElementById("show abilities").style.zIndex = "-1";
+    document.getElementById("show project").style.zIndex = "-1";
+    document.getElementById("show contact").style.zIndex = "-1";
+  }
+function show_abilities() {
+    document.getElementById("show profile").style.zIndex = "-1";
+    document.getElementById("show experiences").style.zIndex = "-1";
+    document.getElementById("show abilities").style.zIndex = "2";
+    document.getElementById("show project").style.zIndex = "-1";
+    document.getElementById("show contact").style.zIndex = "-1";
+  }
+function show_project() {
+    document.getElementById("show profile").style.zIndex = "-1";
+    document.getElementById("show experiences").style.zIndex = "-1";
+    document.getElementById("show abilities").style.zIndex = "-1";
+    document.getElementById("show project").style.zIndex = "2";
+    document.getElementById("show contact").style.zIndex = "-1";
+  }
+function show_contact() {
+    document.getElementById("show profile").style.zIndex = "-1";
+    document.getElementById("show experiences").style.zIndex = "-1";
+    document.getElementById("show abilities").style.zIndex = "-1";
+    document.getElementById("show project").style.zIndex = "-1";
+    document.getElementById("show contact").style.zIndex = "2";
+  }
